0c30c4c0f1436917686f1b2004fd58fee0f8bd4d
rabius-sunny <rabiussunnyc3x@gmail.com>
2023-12-03 23:16:17 +0600

    cleanup and ui-components

--- configs/site.ts
+++ configs/site.ts
@@ -1,13 +1,14 @@
 export type TSiteInfo = typeof siteInfo
 export type TNavItem = typeof siteInfo.navItems
 export const siteInfo = {
-  name: 'My App',
-  description: 'Description about My App',
+  name: 'রক্তদাতা',
+  description: 'ব্লাড ডোনেশন এপ্লিকেশন',
   authors: {
     url: 'https://fb.com/rabibinsalam',
     name: 'Rabius Sunny',
   },
-  generator: 'Meta generator word for My App',
+  generator:
+    'রক্তদাতা রক্ত দাতা ব্লাড ডোনেশন রক্তদান রক্তগ্রহণ blood blood-donation donation donate blood blood-groups',
   navItems: [
     { name: 'Home', href: '/' },
     { name: 'About Us', href: '/about-us' },
