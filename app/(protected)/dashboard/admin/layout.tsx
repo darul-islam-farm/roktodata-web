3e2185462acefb95066093bb1148888bc8ac5fc5
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-03 21:30:05 +0600

    chore: remove admin auth redirect from layout

--- app/(protected)/dashboard/admin/layout.tsx
+++ app/(protected)/dashboard/admin/layout.tsx
@@ -1,4 +1,3 @@
-import { redirect } from 'next/navigation'
 import { auth } from '@/configs/auth'
 
 import { Toaster } from '@/components/ui/sonner'
@@ -6,8 +5,6 @@ import PanelLayout from '@/components/shared/PanelLayout'
 
 export default async function AdminLayout({ children }: IChildren) {
   const session = await auth()
-  /** @TODO redirect unAuth */
-  // if (session?.user.role !== 'ADMIN') redirect('/auth/login')
   return (
     <PanelLayout session={session} admin>
       <Toaster closeButton richColors expand />
