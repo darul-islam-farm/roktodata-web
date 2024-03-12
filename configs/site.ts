9e1780cb7ecc4227129c074d9a4ddf923fc31122
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-03-13 01:02:02 +0600

    chore: endpoint for azure

--- configs/site.ts
+++ configs/site.ts
@@ -111,8 +111,7 @@ export const siteInfo = {
   ]
 }
 
-export const api = 'http://localhost:4000'
-// export const api =
-//   process.env.NODE_ENV === 'development'
-//     ? 'http://localhost:3000'
-//     : 'https://roktodata.onrender.com'
+export const api =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000'
+    : 'https://roktodata.com'
