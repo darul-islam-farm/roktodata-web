f6efc018af58a1cb573fd32fff38b6953630e1d9
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-05-15 02:28:49 +0600

    update: forum page on navigation

--- app/(basic)/(navigation)/forums/page.tsx
+++ app/(basic)/(navigation)/forums/page.tsx
@@ -1,3 +1,12 @@
+import Forums from '@/components/home/Forums'
+import Container from '@/components/shared/Container'
+
 export default function Forum() {
-  return <div>Forum</div>
+  return (
+    <Container>
+      <h1 className='text-center my-8'>ফোরাম থেকে পড়ুন</h1>
+
+      <Forums />
+    </Container>
+  )
 }
