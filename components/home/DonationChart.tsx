b680506e3c568347bf642e99e48fed65f1f16d86
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-05-15 12:46:37 +0600

    update: donation chart image fine-tuning

--- components/home/DonationChart.tsx
+++ components/home/DonationChart.tsx
@@ -13,17 +13,21 @@ export default function DonationChart() {
 
   return (
     <div>
-      <Container>
+      <Container size='md'>
         <SectionHeader title='রক্তদানের চার্ট' />
         <div className='hidden md:block'>
-          <div className='grid grid-cols-3 xl:grid-cols-4 gap-x-2 sm:gap-x-4 gap-y-6 place-items-center'>
+          <div className='grid grid-cols-3 xl:grid-cols-4 gap-6 place-items-center'>
             {Array.from({ length: 8 }, (count: number) => count).map(
               (_num, idx) => (
-                <div key={idx}>
+                <div
+                  key={idx}
+                  className='bg-white p-2 lg:px-6 lg:py-4 rounded-xl shadow-xl group'
+                >
                   <Image
                     src={`/images/others/${idx + 1}.svg`}
-                    width={250}
+                    width={200}
                     height={280}
+                    className='group-hover:scale-110 duration-150'
                     alt='blood donation chart'
                   />
                 </div>
@@ -45,16 +49,20 @@ export default function DonationChart() {
               delay: 2000,
               disableOnInteraction: true
             }}
+            spaceBetween={10}
           >
             {Array.from({ length: 8 }, (count: number) => count).map(
               (_num, idx) => (
                 <SwiperSlide key={idx} className='pb-8'>
-                  <Image
-                    src={`/images/others/${idx + 1}.svg`}
-                    width={250}
-                    height={280}
-                    alt='blood donation chart'
-                  />
+                  <div className='bg-white p-2 rounded-lg group'>
+                    <Image
+                      src={`/images/others/${idx + 1}.svg`}
+                      width={200}
+                      height={250}
+                      className='w-full group-hover:scale-105 duration-150'
+                      alt='blood donation chart'
+                    />
+                  </div>
                 </SwiperSlide>
               )
             )}
